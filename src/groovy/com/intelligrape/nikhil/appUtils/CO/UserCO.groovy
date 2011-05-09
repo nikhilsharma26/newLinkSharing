c5ce237d1ef14f15a06df0a7b9ef6b171e7e954c
Nikhil Sharma <nikhil@intelligrape.com>
2011-05-09 17:24:35 +0530

    eliminated duplicate friends when saving in my database

--- src/groovy/com/intelligrape/nikhil/appUtils/CO/UserCO.groovy
+++ src/groovy/com/intelligrape/nikhil/appUtils/CO/UserCO.groovy
@@ -9,13 +9,13 @@ class UserCO {
 
     void saveUserFromCO(User user) {
         UserFriend.withNewSession {
-            if(User.findByFacebookId(this.facebookId)) {
-                log.error("Already have this friend")
+            if (UserFriend.findByFacebookId(this.facebookId)) {
+                println("!!!!!!Already have this friend")
             }
             else {
 //                user.addToFriends(userFriend)
-                UserFriend userFriend = new UserFriend(facebookId:this.facebookId, name:this.name, user:user )
-                userFriend.save(flush:true)
+                UserFriend userFriend = new UserFriend(facebookId: this.facebookId, name: this.name, user: user)
+                userFriend.save(flush: true)
                 println("Saved this friend ${this.name}")
             }
         }
