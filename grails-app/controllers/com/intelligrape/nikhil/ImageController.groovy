b5b57a4a9db851d6f2858dc2a46ccca9e1f0716b
Nikhil Sharma <nikhil@intelligrape.com>
2011-05-10 19:33:04 +0530

    added image controller to display images by fetching them from their id

---
+++ grails-app/controllers/com/intelligrape/nikhil/ImageController.groovy
@@ -0,0 +1,13 @@
+package com.intelligrape.nikhil
+
+class ImageController {
+
+    def index = { }
+
+    def showImage = {
+        println "#### in show Image"
+        def image = ChildContact.findById(params.imageId).image
+//        response.setContentType(ChildContact.findById(params.id).type)
+        response.outputStream << image
+    }
+}
